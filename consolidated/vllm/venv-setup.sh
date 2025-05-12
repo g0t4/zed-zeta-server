9a852a0485e4c09932cf3405f54ab611187952bc
Wes Higbee <wes.mcclure@gmail.com>
2025-05-12 18:23:04 -0500

    setup venv with custom vllm build

---
+++ consolidated/vllm/venv-setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env fish
+
+cd "$HOME/repos/github/g0t4/zed-zeta-server" || exit
+
+# * new .venv
+rm -rf .venv # wipe out venv to be safe
+uv venv
+source .venv/bin/activate.fish
+
+# * vllm deps:
+uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+uv pip install -r ~/repos/github/vllm-project/vllm-latest/requirements/build.txt
+
+export MAX_JOBS=(nproc)
+uv pip install -e "$HOME/repos/github/vllm-project/vllm-latest" --no-build-isolation
+
+# * zed-zeta-server deps:
+uv pip install -r requirements.txt
+
+# * this was in notes for vllm itself, its build.. not sure I need this here?
+# uv pip install transformers # no direct torch deps
