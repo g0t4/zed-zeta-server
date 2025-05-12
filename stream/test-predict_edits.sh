c06b820a535eb5d98b70cfeb46826c5b41bddbd9
Wes Higbee <wes.mcclure@gmail.com>
2025-05-12 17:15:50 -0500

    and that means moving this to body too which I wanted

--- stream/test-predict_edits.sh
+++ stream/test-predict_edits.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-http --pretty colors localhost:7100/stream_edits?include_finish_reason=1 <<'EOF'
+http --pretty colors localhost:7100/stream_edits <<'EOF'
 {
     "outline": "```lua/ask-openai/prediction/tests/calc/calc.lua\nfunction M.add\n```\n",
     "input_events": "User edited \"lua/ask-openai/prediction/tests/calc/calc.lua\":\n```diff\n@@ -7,4 +7,5 @@\n \n \n \n+\n return M\n\n```\n\nUser edited \"lua/ask-openai/prediction/tests/calc/calc.lua\":\n```diff\n@@ -8,4 +8,5 @@\n \n \n \n+\n return M\n\n```",
     "input_excerpt": "```ask-openai.nvim/lua/ask-openai/prediction/tests/calc/calc.lua\n<|start_of_file|>\n<|editable_region_start|>\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n<|user_cursor_is_here|>\n\n\n\n\n\nreturn M\n\n<|editable_region_end|>\n```",
     "speculated_output": "<|editable_region_start|>\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n<|user_cursor_is_here|>\n\n\n\n\n\nreturn M\n\n<|editable_region_end|>",
     "can_collect_data": false,
-    "diagnostic_groups": []
+    "diagnostic_groups": [],
+    "include_finish_reason": true
 }
 EOF
 
